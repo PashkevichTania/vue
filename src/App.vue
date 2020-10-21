75257b378c64aec293b0db74804c23cac12651f9
PashkevichTania <43377440+PashkevichTania@users.noreply.github.com>
2020-10-21 12:07:20 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Hello world"/>
+    <Comp msg="VUE"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Comp from './components/comp.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Comp
+  }
+}
+
+
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
